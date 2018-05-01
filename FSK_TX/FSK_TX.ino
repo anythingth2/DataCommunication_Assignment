f07b2a5aea4bfe0978ff3a46016fba68e5fac1b0
ChiChaChai <chatchaishaetan@gmail.com>
2018-05-01 07:09:49 +0700

    refactor fsk tx

--- FSK_TX/FSK_TX.ino
+++ FSK_TX/FSK_TX.ino
@@ -18,16 +18,15 @@ uint16_t S_DAC[SAMPLING] = {2048, 4095, 2048, 0};
 unsigned long frameTime = 510000 + 50000;
 Adafruit_MCP4725 dac;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   dac.begin(0x64);
 
-  delay0 = 1000000 / 50  - defaultFreq;
-  delay1 = 1000000 / 75  - defaultFreq;
-  delay2 = 1000000 / 100  - defaultFreq;
+  delay0 = 1000000 / 50 - defaultFreq;
+  delay1 = 1000000 / 75 - defaultFreq;
+  delay2 = 1000000 / 100 - defaultFreq;
   delay3 = 1000000 / 125 - defaultFreq;
-
-
 
   Serial.begin(115200);
   Serial.flush();
@@ -43,92 +42,91 @@ void setup() {
   Serial.println();
   dac.setVoltage(0, false);
 
-  for (int i = 0; i < SAMPLING; i++) {
-    S_DAC[i] = (sin(2 * PI / SAMPLING * i ) + 1) * 2047 ;
-    //    Serial.print("S_DAC = ");
-    //    Serial.println(S_DAC[i]);
+  for (int i = 0; i < SAMPLING; i++)
+  {
+    S_DAC[i] = (sin(2 * PI / SAMPLING * i) + 1) * 2047;
   }
-
-  
 }
-void generateWave(int freqency) {
-  //  Serial.print("Start Generate Wave ");
-  //  Serial.print(freqency);
-  int numWave =  freqency / 250 * 8;
-  //  numWave = 1;
-  int delayTime = (10000000 / freqency  - 1 / defaultFreq) / SAMPLING ;
-  //  delayTime /=10;
-  //  Serial.print("delay ");
+void generateWave(int freqency)
+{
 
-  //  Serial.print("\tdelayTime ");
-  //  Serial.print(delayTime);
-  Serial.print("\tnumWave ");
-  Serial.println(numWave);
-  Serial.print("Delay ");
-  Serial.println(delayTime);
-  Serial.print("elapsedTime : ");
+  int numWave = freqency / 250 * 8;
+
+  int delayTime = (10000000 / freqency - 1 / defaultFreq) / SAMPLING;
+
   unsigned long elapsedTime = micros();
-  for (int i = 0; i < numWave; i++) {
-    for (int j = 0; j < SAMPLING; j++) {
+  for (int i = 0; i < numWave; i++)
+  {
+    for (int j = 0; j < SAMPLING; j++)
+    {
 
       dac.setVoltage(S_DAC[j], false);
       delayMicroseconds(delayTime);
-
     }
   }
-  
 
   long waitForFullFrame = frameTime - (micros() - elapsedTime);
-  //  if (waitForFullFrame > 0)
-  while (waitForFullFrame > 0) {
-    if (waitForFullFrame > 16000) {
+
+  while (waitForFullFrame > 0)
+  {
+    if (waitForFullFrame > 16000)
+    {
       delayMicroseconds(16000);
       waitForFullFrame -= 16000;
-    } else {
-      delayMicroseconds(waitForFullFrame); break;
     }
+    else
+    {
+      delayMicroseconds(waitForFullFrame);
+      break;
+    }
+  }
+}
 
+void sendByte(int inByte)
+{
+
+  Serial.print(inByte);
+  Serial.print(" -> ");
+  int input[4];
+  for (int i = 3; i >= 0; i--)
+  {
+    input[i] = inByte & 3;
+    Serial.print(input[i], BIN);
+    Serial.print(" ");
+    inByte >>= 2;
+  }
+  Serial.println();
+  for (int k = 3; k >= 0; k--)
+  {
+    if (input[k] == 0)
+    {
+      generateWave(f0);
+    }
+    else if (input[k] == 1)
+    {
+      generateWave(f1);
+    }
+    else if (input[k] == 2)
+    {
+      generateWave(f2);
+    }
+    else if (input[k] == 3)
+    {
+      generateWave(f3);
+    }
   }
 
-  elapsedTime = micros() - elapsedTime;
-  Serial.println(elapsedTime);
-  
-  //  Serial.println("\t Generated!");
+  delayMicroseconds(5000);
 }
 int in = 0;
-int input[4];
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  if (Serial.available() > 0) {
+void loop()
+{
+  if (Serial.available() > 0)
+  {
     in = Serial.parseInt();
-
-    //    Serial.write(in);
-    if (in == 0)return;
-    Serial.print(in);
-    Serial.print(" -> ");
-
-    for (int i = 3; i >= 0; i--) {
-      input[i] = in & 3;
-      Serial.print(input[i], BIN);
-      Serial.print(" ");
-      in >>= 2;
-    }
-    Serial.println();
-    for (int k = 3; k >= 0; k--) {
-      if (input[k] == 0) {
-        generateWave(f0);
-      } else if (input[k] == 1) {
-        generateWave(f1);
-      } else if (input[k] == 2) {
-        generateWave(f2);
-      } else if (input[k] == 3) {
-        generateWave(f3);
-      }
-    }
-
-    delayMicroseconds(5000);
+    if (in == 0)
+      return;
+    sendByte(in);
   }
-
-//  dac.setVoltage(S_DAC[SAMPLING-1], false);
 }
